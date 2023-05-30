8d1c9bd247f33b3f764e81f73165e49cf3ccf70f
bheka <bheka.bj@gmail.com>
2023-05-30 15:50:19 +0200

    added a clear button

--- electricity.dom.js
+++ electricity.dom.js
@@ -5,10 +5,18 @@ const unitsAvailable = document.querySelector(".unitsAvailable")
 const totalUnits = document.querySelector(".totalUnits")
 const totalAmount = document.querySelector(".totalAmount")
 const useNow = document.querySelector(".useNow")
+const ClearNow = document.querySelector(".ClearNow")
 
 
+let initial = 0;
 // Factory Function instance 
 const electricity =  Electricity();
+totalUnits.innerHTML=localStorage.getItem("totalunits")||initial.toFixed(2)
+totalAmount.innerHTML=localStorage.getItem("totalamount")||initial.toFixed(2)
+electricity.updateTotals(parseFloat(totalAmount.innerHTML),parseFloat(totalUnits.innerHTML))
+
+
+
 topupNow.addEventListener("click",function(){
     const checkedBtn = document.querySelector('input[name="buyElectricity"]:checked');
     if (checkedBtn) {
@@ -18,19 +26,39 @@ topupNow.addEventListener("click",function(){
         unitsAvailable.innerHTML=electricity.getUnitsAvailable()
         totalUnits.innerHTML=electricity.totalUnitsBought()
         totalAmount.innerHTML=electricity.totalAmountSpent()
+        localStorage.setItem(
+            "totalunits",
+            parseFloat(totalUnits.innerHTML)
+          );
+          localStorage.setItem(
+            "totalamount",
+            parseFloat(totalAmount.innerHTML)
+          );
       }
 });
 useNow.addEventListener("click",function(){
     const checkedButton = document.querySelector('input[name="useElectricity"]:checked');
     if (checkedButton) {
         const selectedValues = checkedButton.value;
-        console.log(selectedValues);
-      
         electricity.useAppliance(selectedValues)  
         unitsAvailable.innerHTML=electricity.getUnitsAvailable()
         totalUnits.innerHTML=electricity.totalUnitsBought()
         totalAmount.innerHTML=electricity.totalAmountSpent()
+        localStorage.setItem(
+            "totalunits",
+            parseFloat(totalUnits.innerHTML)
+          );
+          localStorage.setItem(
+            "totalamount",
+            parseFloat(totalAmount.innerHTML)
+          );
       }
 });
 
-// DOM events here 
+// DOM events here
+ClearNow.addEventListener("click",function(){
+     localStorage.clear()
+     unitsAvailable.innerHTML=0
+        totalUnits.innerHTML=initial.toFixed(2)
+        totalAmount.innerHTML=initial.toFixed(2)
+}); 
--- electricity.js
+++ electricity.js
@@ -11,6 +11,7 @@ function Electricity() {
     let totalAmount = 0;
     //initialize a variable to zero to keep track of ALL the units used
     let totalUnits = 0;
+    let advancePaid = false;
     // do we want to go with this or array? 
     let appliances = {
         'Stove': 10, 
@@ -19,42 +20,48 @@ function Electricity() {
         'Fridge': 13
     };
 
+ 
     function topUpElectricity(amount) {
-        //get all the money that was spent
         if(amount!='advance'){
-            totalAmount+=parseFloat(amount)
-        }
-        
-        if((amount-theAdvancedue)>=0){
-            amountTracker = (amount-theAdvancedue)
-            theAdvancedue=0;
-        }else if((amount-theAdvancedue)<0){
-            theAdvancedue = (amount-theAdvancedue)*-1
-            amountTracker=0;
-        }
-      
-        if(amountTracker >= 50){
-            unitsAvailable += 35;
-            totalUnits+=35;
-            theAdvance=0;
-        }else if(amountTracker >= 20){
-          if(theAdvancedue===0){
-            unitsAvailable += 14;
-            totalUnits+=14;
-            theAdvance=0;
+             totalAmount+=parseFloat(amount)
+            }
+        if (amount === 'advance') {
+          if (theAdvance === 0) {
+            theAdvancedue += 30;
+            theAdvance = 21;
+            unitsAvailable += 21;
+            totalUnits += 21;
+           
+            advancePaid=true;
           }
-            
-        }else if(amountTracker >= 10){
-            unitsAvailable += 7;
-            totalUnits+=7;
+        } else {
             theAdvance=0;
-        }else if(amount === 'advance'&&theAdvance===0){
-            theAdvancedue +=30
-            theAdvance = 21; 
-            unitsAvailable += 21 
+          if ((amount - theAdvancedue) >= 0) {
+            amountTracker = (amount - theAdvancedue);
+            theAdvancedue = 0;
+           
+          } else if ((amount - theAdvancedue) < 0) {
+            theAdvancedue = (theAdvancedue - amount);
+            amountTracker = 0;
             
+          }
+      
+          if (amountTracker >= 50) {
+            unitsAvailable += 35;
+            totalUnits += 35;
+            
+          } else if (amountTracker >= 20) {
+            unitsAvailable += 14;
+            totalUnits += 14;
+           
+          } else if (amountTracker >= 10) {
+            unitsAvailable += 7;
+            totalUnits += 7;
+            
+          }
         }
-    }
+      }
+      
 
     function getUnitsAvailable() {
          return unitsAvailable;
@@ -86,10 +93,8 @@ function Electricity() {
     }
 
     function advanceTaken() {
-        console.log(theAdvance)
-        if(theAdvance>0){
-            return true
-        }
+
+        return advancePaid
     }
 
     function totalAmountSpent() {
@@ -99,6 +104,10 @@ function Electricity() {
     function totalUnitsBought(){
         return totalUnits
     }
+    function updateTotals(totalA,totalU){
+        totalAmount=totalA;
+        totalUnits=totalU;
+    }
 
     return {
         advanceTaken,
@@ -106,7 +115,8 @@ function Electricity() {
         getUnitsAvailable,
         useAppliance,
         totalAmountSpent,
-        totalUnitsBought
+        totalUnitsBought,
+        updateTotals
 
     }
 }
