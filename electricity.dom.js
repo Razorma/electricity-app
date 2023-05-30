9586363be4e5baf45b5604727aafd2b8f7797b97
bheka <bheka.bj@gmail.com>
2023-05-30 14:17:50 +0200

    wrote the top up function

--- electricity.dom.js
+++ electricity.dom.js
@@ -1,9 +1,23 @@
 // DOM element(s) references
 
+const topupNow = document.querySelector(".topupNow")
+const unitsAvailable = document.querySelector(".unitsAvailable")
+const totalUnits = document.querySelector(".totalUnits")
+const totalAmount = document.querySelector(".totalAmount")
 
 
 // Factory Function instance 
 const electricity =  Electricity();
-
+topupNow.addEventListener("click",function(){
+    const checkedBtn = document.querySelector('input[name="buyElectricity"]:checked');
+    if (checkedBtn) {
+        const selectedValue = checkedBtn.value;
+        // console.log(selectedValue);
+        electricity.topUpElectricity(selectedValue)
+        unitsAvailable.innerHTML=electricity.getUnitsAvailable()
+        totalUnits.innerHTML=electricity.totalUnitsBought()
+        totalAmount.innerHTML=electricity.totalAmountSpent()
+      }
+});
 
 // DOM events here 
--- electricity.js
+++ electricity.js
@@ -1,5 +1,16 @@
 function Electricity() {
-
+    //initialize a variable to zero to store the units
+    let unitsAvailable = 0;
+    //initialize a variable to zero to keep track of the advance
+    let theAdvance = 0;
+    //initialize a variable to zero to keep track of the amount you owe
+    let theAdvancedue = 0;
+    //initialize a variable to zero to keep help keep track of the amount that is left after advance deductions
+    let amountTracker = 0;
+    //initialize a variable to zero to keep track of ALL the money used
+    let totalAmount = 0;
+    //initialize a variable to zero to keep track of ALL the units used
+    let totalUnits = 0;
     // do we want to go with this or array? 
     let appliances = {
         'Stove': 10, 
@@ -9,8 +20,41 @@ function Electricity() {
     };
 
     function topUpElectricity(amount) {
+        //get all the money that was spent
+        if(amount!='advance'){
+            totalAmount+=parseFloat(amount)
+        }
+        
+        if((amount-theAdvancedue)>=0){
+            amountTracker = (amount-theAdvancedue)
+            theAdvancedue=0;
+          
+        }else if((amount-theAdvancedue)<0){
+            theAdvancedue = (amount-theAdvancedue)*-1
+            amountTracker=0;
+           
+        }
+      
+        if(amountTracker >= 50){
+            unitsAvailable += 35;
+            totalUnits+=35;
+         
+        }else if(amountTracker >= 20){
+      
+            unitsAvailable += 14;
+            totalUnits+=14;
 
-
+          
+        }else if(amountTracker >= 10){
+            unitsAvailable += 7;
+            totalUnits+=7;
+       
+        }else if(amount === 'advance'&&theAdvance===0){
+            theAdvancedue +=30
+            theAdvance = 21; 
+            unitsAvailable += 21 
+            
+        }
     }
 
     function getUnitsAvailable() {
@@ -26,12 +70,18 @@ function Electricity() {
     }
 
     function advanceTaken() {
+        console.log(theAdvance)
+        if(theAdvance>0){
+            return true
+        }
     }
 
     function totalAmountSpent() {
+        return totalAmount
     }
 
     function totalUnitsBought(){
+        return totalUnits
     }
 
     return {
